58fc2b5c111ba521fc4c8dd9c112ae71df11ca14
imam-hossain <zunayedemon6t9@gmail.com>
2023-08-29 15:28:39 +0600

    create courses or enrollment tabler

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -14,4 +14,19 @@ CREATE TABLE students (
     status VARCHAR(50)
 );
 
+-- Create courses table
+CREATE TABLE courses (
+    course_id INT PRIMARY KEY,
+    course_name VARCHAR(255),
+    credits INT
+);
+
+-- Create enrollment table
+CREATE TABLE enrollment (
+    enrollment_id INT PRIMARY KEY,
+    student_id INT,
+    course_id INT,
+    FOREIGN KEY (student_id) REFERENCES students(student_id),
+    FOREIGN KEY (course_id) REFERENCES courses(course_id)
+);
 
