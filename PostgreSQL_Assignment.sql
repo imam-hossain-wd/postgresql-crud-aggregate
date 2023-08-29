0482507904a6a537e1f8ccfeb30ffe5787a5b8f1
imam-hossain <zunayedemon6t9@gmail.com>
2023-08-29 16:20:48 +0600

    query 5 done

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -249,3 +249,12 @@ WHERE NOT EXISTS (
     FROM enrollment
     WHERE enrollment.course_id = courses.course_id
 );
+
+--query-5 : Retrieve the names of students using a limit of 2, starting from the 3rd student.
+
+SELECT student_name
+FROM students
+ORDER BY student_id
+LIMIT 2 OFFSET 2;
+
+SELECT * FROM students
