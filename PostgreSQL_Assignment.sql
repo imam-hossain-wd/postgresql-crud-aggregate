f0fd1134a3c488d01b02d5cdaafd323826a27011
imam-hossain <zunayedemon6t9@gmail.com>
2023-08-29 15:24:33 +0600

    create a student table

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,3 +1,17 @@
+-- Active: 1692970806409@@127.0.0.1@5432@university_db@public
 
+-- create a databse
 CREATE DATABASE university_db
+
+-- create students table
+CREATE TABLE students (
+    student_id SERIAL PRIMARY KEY,
+    student_name VARCHAR(255),
+    age INTEGER,
+    email VARCHAR(255),
+    frontend_mark INTEGER,
+    backend_mark INTEGER,
+    status VARCHAR(50)
+);
+
 
